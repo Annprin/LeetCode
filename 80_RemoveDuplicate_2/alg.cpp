9b3fe4b9e40fd34a07e4ee2f716dfdca38a031ad
Annprin <Ancka.korchagina2017@yandex.ru>
2025-03-17 23:09:54 +0300

    done delete element 2

--- 80_RemoveDuplicate_2/alg.cpp
+++ 80_RemoveDuplicate_2/alg.cpp
@@ -5,14 +5,24 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 1, last;
-        if (nums.size() > 0)
+        int i = 1, last, count = 0;
+        if (nums.size() > 0){
             last = nums[0];
+            count = 1;
+        }
         for (int ind = 1; ind < nums.size(); ++ind){
             if (nums[ind] != last){
                 nums[i] = nums[ind];
                 last = nums[ind];
                 ++i;
+                count = 1;
+            }
+            else{
+                ++count;
+                if (count < 3){
+                    nums[i] = nums[ind];
+                    ++i;
+                }
             }
         }
         return i;
